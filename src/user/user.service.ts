ba47236429136228d06e7ae307f87b5943f1ddfd
FahadQ <fhdq2003@gmail.com>
2025-09-24 11:35:22 +0300

    fixed error

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,7 +3,7 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm/browser';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService
