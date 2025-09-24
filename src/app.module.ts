dcabc3a813f7460216c52ed8c9de032cdb794e69
FahadQ <fhdq2003@gmail.com>
2025-09-24 09:31:57 +0300

    added cors, dotenv, and postgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(
+      {
+        isGlobal: true,
+        envFilePath: '.env',
+      }
+    ),
+    TypeOrmModule.forRoot(
+      {
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        password: process.env.PASSWORD,
+        username: 'postgres',
+        entities: [],
+        database: 'projectDB',
+        synchronize: true,
+        logging: true,
+      }
+    ),
+    UserModule
+  ],
+  controllers: [ AppController ],
+  providers: [ AppService ],
 })
-export class AppModule {}
+export class AppModule { }
