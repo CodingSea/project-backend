88e51def0a6aedea95c80fb450e088d27bd15e96
FahadQ <fhdq2003@gmail.com>
2025-10-01 09:42:01 +0300

    added update dto to user

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,4 +1,37 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+import 
+{
+    IsString,
+    IsAlphanumeric,
+    IsEmail,
+    IsEnum,
+    IsInt,
+    IsNotEmpty,
+    isString,
+    Matches,
+    MinLength
+} from "class-validator";
+
+export class UpdateUserDto extends PartialType(CreateUserDto) 
+{
+    @IsString()
+    @MinLength(2, { message: "First Name must have atleast 2 characters." })
+    @IsNotEmpty()
+    first_name: string;
+
+    @IsNotEmpty()
+    @MinLength(2, { message: "First Name must have atleast 2 characters." })
+    last_name: string;
+
+    @IsNotEmpty()
+    @IsEmail({}, { message: 'Please provide valid Email.' })
+    email: string;
+
+    @IsString()
+    @IsEnum([ "admin", 'developer' ])
+    role: string;
+
+    
+}
